d78cb09bc6952a7f148b5e24a47f3a7428b28728
Crazy Overdose <alena23062000@gmail.com>
2020-08-25 17:18:55 +0300

    Final version

---
+++ coursera/Part_of_the_set.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+template <typename T>
+vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
+	auto iter = find_if(elements.begin(), elements.end(), [border](T i)
+	{ if (i > border)
+		return true;
+	return false;
+	});
+
+	vector<T> result;
+	while (iter != elements.end()) {
+		result.push_back(*iter);
+		++iter;
+	}
+
+	return result;
+}
+
+//int main() {
+//	for (int x : FindGreaterElements(set<int>{1, 5, 7, 8}, 5)) {  // 7 8
+//		cout << x << " ";
+//	}
+//	cout << endl;
+//
+//	string to_find = "Python";
+//	cout << FindGreaterElements(set<string>{"C", "C++"}, to_find).size() << endl; // 0
+//	return 0;
+//}
